5e4e1286e004c6b0c1016cd94843e1332580d2d2
Evan <evanaltwarg2021@gmail.com>
2024-01-20 13:56:38 -0500

    fix: :bug: fix path alias import in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
+import path from "path";
 
-export default defineConfig({});
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
