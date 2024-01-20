caf33295c59748c3b7f47ca058b9362d32abc617
Evan <evanaltwarg2021@gmail.com>
2024-01-20 02:20:38 -0500

    feat: :goal_net: Add loading state on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { type Component } from "solid-js";
+import { Show, type Component } from "solid-js";
 import { Header } from "./components/Header";
 import { AppStateLoader } from "./components/AppStateLoader";
 import GuessForm from "./components/GuessForm";
+import AppStore from "./components/AppStore";
 
 const App: Component = () => {
+  const { appState } = AppStore;
   return (
     <AppStateLoader>
       <div class="font-roboto h-screen w-screen bg-neutral-800 overflow-y-auto flex items-center pt-8 overflow-x-hidden flex-col text-white gap-4">
         <Header />
+
         <div class="p-4 md:p-8 rounded border-neutral-600 border-2">
           <span class=" text-xl md:text-4xl">
             Guess today's World of Tanks vehicle!
           </span>
         </div>
-        <GuessForm />
+        <Show
+          when={appState.tankList.length > 0}
+          fallback={<div>Loading... Please wait</div>}
+        >
+          <GuessForm />
+        </Show>
       </div>
     </AppStateLoader>
   );
