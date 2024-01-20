c8e63fd63c8048e546d31a1995ca429eed337e41
Evan <evanaltwarg2021@gmail.com>
2024-01-20 14:06:05 -0500

    style: :lipstick: center guess form and guess list

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,18 @@ import GuessList from "@/components/GuessList";
 
 export default function Home() {
   return (
-    <main>
+    <main class="grid justify-center gap-4">
       <div class="p-4 md:p-8 rounded border-neutral-600 border-2">
         <span class=" text-xl md:text-4xl">
           Guess today's World of Tanks vehicle!
         </span>
       </div>
-      <GuessForm />
-      <GuessList />
+      <div class="flex justify-center">
+        <GuessForm />
+      </div>
+      <div class="flex justify-center">
+        <GuessList />
+      </div>
     </main>
   );
 }
