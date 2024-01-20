3bd8afe0ce3fe254e202a7c90cf2f240255899b2
Evan <evanaltwarg2021@gmail.com>
2024-01-19 22:02:23 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div class="h-screen w-screen bg-neutral-800 overflow-y-auto">
+      <div class="flex">hi</div>
+    </div>
+  );
+};
+
+export default App;
