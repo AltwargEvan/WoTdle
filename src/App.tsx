3ec7161d9b53cf4a248cde17b242a30d7ba8a3ab
Evan <evanaltwarg2021@gmail.com>
2024-01-20 22:01:34 -0500

    refactor: :recycle: refactor root back into inline function

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import { type Component, Suspense, JSXElement } from "solid-js";
-// import { AppStateLoader } from "./components/AppStateLoader";
-// import GuessForm from "./components/GuessForm";
-import "./app.css";
+import { Suspense } from "solid-js";
 import { FileRoutes } from "@solidjs/start";
-import { RouteSectionProps, Router } from "@solidjs/router";
+import { Router } from "@solidjs/router";
 import Nav from "./components/Nav";
+import "./app.css";
 
-const Root: Component<RouteSectionProps<unknown>> = (props) => {
-  return (
-    <div class="font-roboto h-screen w-screen bg-neutral-800 overflow-y-auto flex items-center pt-8 overflow-x-hidden flex-col text-white gap-4">
-      <Nav />
-      <Suspense>{props.children}</Suspense>
-    </div>
-  );
-};
 const App = () => {
   return (
-    <Router root={Root}>
+    <Router
+      root={(props) => (
+        <div class="font-roboto h-screen w-screen bg-neutral-800 overflow-y-auto flex items-center pt-8 overflow-x-hidden flex-col text-white gap-4">
+          <Nav />
+          <Suspense>{props.children}</Suspense>
+        </div>
+      )}
+    >
       <FileRoutes />
     </Router>
   );
