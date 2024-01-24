b697aaabb6ff77520d3d0e42e08c286399ad2158
Evan <evanaltwarg2021@gmail.com>
2024-01-24 10:44:32 -0500

    refactor: :art: remove library solid icons. replace icons with inline svgs

--- src/components/GuessForm.tsx
+++ src/components/GuessForm.tsx
@@ -1,4 +1,3 @@
-import { BiSolidRightArrow } from "solid-icons/bi";
 import { Component, For, Show, createSignal } from "solid-js";
 import AppStore from "./AppStore";
 import Fuse, { FuseResult } from "fuse.js";
@@ -54,7 +53,17 @@ const GuessForm: Component = () => {
           value={guess()}
         />
         <button class="border-neutral-600 border-t-2 border-r-2 border-b-2 rounded-tr px-3 hover:bg-stone-800">
-          <BiSolidRightArrow size={48} />
+          <svg
+            fill="currentColor"
+            stroke-width="0"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            height="48"
+            width="48"
+            style="overflow: visible; color: currentcolor;"
+          >
+            <path d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z"></path>
+          </svg>
         </button>
       </form>
       <Show when={searchResults()?.length === 0}>
