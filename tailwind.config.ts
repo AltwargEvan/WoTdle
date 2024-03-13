5e6c9dfaf2583e204c3f45b83e41be7ffac8deac
Evan <evanaltwarg2021@gmail.com>
2024-03-13 00:20:29 -0400

    Extend TW theme with colors for correct and partialCorrect

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,10 @@ const config = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        correct: "#538d4e",
+        partialCorrect: "#b59f3b",
+      },
       fontFamily: {
         roboto: ["Roboto Condensed", "sans-serif"],
       },
