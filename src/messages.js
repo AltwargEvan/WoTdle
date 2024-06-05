2e75333cc7c7510cea89940c04884643d9a3d775
Evan <evanaltwarg2021@gmail.com>
2024-06-05 01:05:15 -0400

    Init paraglide

---
+++ src/messages.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import { languageTag } from "./runtime.js"
+import * as en from "./messages/en.js"
+import * as pl from "./messages/pl.js"
+import * as de from "./messages/de.js"
+
+
---
+++ src/messages/en.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+/** 
+ * This file contains language specific message functions for tree-shaking. 
+ * 
+ *! WARNING: Only import messages from this file if you want to manually
+ *! optimize your bundle. Else, import from the `messages.js` file. 
+ * 
+ * Your bundler will (in the future) automatically replace the index function 
+ * with a language specific message function in the build step. 
+ */
+
+
+export {};
