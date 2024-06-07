958d9e7227a3a319edb75fc12d039d3fa8969231
Evan <evanaltwarg2021@gmail.com>
2024-06-06 23:29:51 -0400

    Split longer promt titles into 2 lines

--- src/components/PromptPanel.tsx
+++ src/components/PromptPanel.tsx
@@ -1,14 +1,26 @@
-import { Show } from "solid-js";
+import { Show, createMemo } from "solid-js";
 import { HintPanel } from "./HintPanel";
 import { usePersistedData } from "@/stores/wotdlePersistedDataStore";
 import * as m from "@/paraglide/messages.js";
+import { splitInto2Lines } from "@/utils/splitInto2Lines";
 
 const PromptPanel = () => {
   const [data] = usePersistedData();
+  const title = createMemo(() => {
+    const data = splitInto2Lines(m.prompt_title(), 50);
+    if (data.length === 1) return <h3>{data.at(0)}</h3>;
+    else
+      return (
+        <>
+          <h3 class="-my-1 pt-1">{data.at(0)}</h3>
+          <h3>{data.at(1)}</h3>
+        </>
+      );
+  });
 
   return (
     <div class="w-full sm:w-[450px] px-4 grid items-center bg-neutral-900 rounded border-neutral-600 border p-4 ">
-      <h3 class="text-xl text-center pb-2">{m.prompt_title()}</h3>
+      <div class="text-xl text-center pb-2">{title()}</div>
       <Show
         when={data.dailyVehicleGuesses.length > 0}
         fallback={
